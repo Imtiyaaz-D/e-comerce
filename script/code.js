568ba8b1f50661fdb471dcf21250072cdc32384e
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-31 07:31:31 +0200

    content

---
+++ script/code.js
@@ -0,0 +1,40 @@
+
+
+
+let products = [
+    { id: 1,
+        Name:"",
+        Description:"",
+        Price:"",
+        Image:"https://i.postimg.cc/TwfDK0TT/angiewoodcreationsco-wooden-bracelet-mens-women-s-wood-bracelets-gift-idea-for-him-her-birthday-anni.jpg",
+    
+    },
+    { id: 2,
+        Name:"",
+        Description:"",
+        Price:"",
+        Image:"https://i.postimg.cc/PqmDf9s6/angiewoodcreations-not-engraved-angie-wood-creations-dark-sandalwood-men-s-watch-with-teal-dial-wood.webp",
+    
+    },
+    { id: 3,
+        Name:"",
+        Description:"",
+        Price:"",
+        Image:"https://i.postimg.cc/Y0SL3S4V/Glide-Vitaly-G-2-1100x.webp",
+    
+    },
+    { id: 4,
+        Name:"",
+        Description:"",
+        Price:"",
+        Image:"https://i.postimg.cc/wMbybdLB/Kickback-Vitaly-SS-v1-1946x.webp",
+    
+    },
+    { id: 5,
+        Name:"",
+        Description:"",
+        Price:"",
+        Image:"https://i.postimg.cc/G2KsGDpG/omega-sailing-bracelet-in-stainless-steel-with-dlc-coating-and-black-rubber-accessories-manfredi-jew.webp"
+    
+    },
+]
